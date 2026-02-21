49b8bcc9b1ab02b41212e4150c71843366498fe7
adi <adisudana@gmail.com>
2026-02-22 05:16:47 +0700

    Fix database SSL config for Vercel

--- models/db.js
+++ models/db.js
@@ -2,19 +2,17 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: process.env.DATABASE_URL?.includes('neon.tech')
+    ? {
+        rejectUnauthorized: false,
+        requestCert: true,
+      }
+    : false,
 });
 
-// Test connection
-pool.on('connect', () => {
-  console.log('Connected to PostgreSQL database');
-});
-
+// Don't exit on error - just log
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
-  process.exit(-1);
 });
 
 module.exports = {
